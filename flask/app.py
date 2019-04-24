af97db8276f9afd927321fb0ecace2ed1b4f92b8
Ezira Yimer Wolle <ezirayw@bu.edu>
2019-04-24 00:12:43 -0400

    Search by movie name

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,14 @@ import requests
 import time
 import urllib
 import json
+from pymongo import MongoClient
+
+#Connect to MovieDetector database and user collection
+client = MongoClient()
+db = client.MovieDetector
+collection = db.users
+
+
 # <<<<<<< HEAD
 # #from flask_oauth import OAuth
 # =======
@@ -14,19 +22,23 @@ import json
 # >>>>>>> 3f5f33a7f8f50875ad8714d83dd94fd939c02b8b
 #https://pythonhosted.org/Flask-OAuth/
 #^ All log-in tutorial
+
+
 app = Flask(__name__)
 
 api_key = 'fa03116693262062589d14a72cc612d0'
 api_url = 'https://api.themoviedb.org/3/'
 
+'''
 class Movie:
-    def __init__(self, title, poster, popularity, release_date, overview):
+    def __init__(self, title):
         self.title = title
-        self.poster = poster
-        self.popularity = popularity
-        self.release_date = release_date
-        self.overview = overview
-        self.myRating = 0
+        #self.poster = poster
+        #self.popularity = popularity
+        #self.release_date = release_date
+        #self.overview = overview
+        #self.myRating = 0
+'''
 
 def get_json(url):
     '''Returns json text from a URL'''
@@ -51,26 +63,17 @@ def index():
 @app.route('/test', methods=['GET','POST'])
 def detect():
     if request.method == 'POST':
-
         result = request.form['id']
 
-        url = "https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=" + result + "&page=1"
-        #url = "https://api.themoviedb.org/3/movie/343611?api_key=fa03116693262062589d14a72cc612d0"
+        url = 'https://api.themoviedb.org/3/search/movie?api_key=fa03116693262062589d14a72cc612d0&page=1&query=' + result
         img_url = 'https://image.tmdb.org/t/p/w500'
-        json = get_json(url)
+        movie_list = get_json(url)
         movies = []
-        for movie in json['results']:
-            movies.append(Movie(movie['title'],
-                                img_url + movie['poster_path'],
-                                movie['popularity'],
-                                movie['release_date'],
-                                movie['overview']))
-        return render_template("test.html", movie)
+        for i in movie_list['results']:
+            movies.append(i['title'])
+        return render_template("test.html", movies = movies)
     else:
         return render_template("test.html")
-# This is searching by name.
-# I cannot get it to work yet, and I do not know why.
-# Searching by ID is DOWN there, and Sumara Get it to work.(I just try adding poster to it not sure if it work.)
 
 """
 def detect():
@@ -103,4 +106,4 @@ def detect():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
