d4d90518ea23c3c3fe9dd1e5073f8d3c783facce
Chanavee Prasopsanti <cprasop@bu.edu>
2019-04-25 16:17:33 -0400

    Add link to log-in on search page

--- flask/app.py
+++ flask/app.py
@@ -109,7 +109,7 @@ def detect():
     if request.method == 'POST':
         result = request.form['title']
 
-        url = 'https://api.themoviedb.org/3/search/movie?api_key=fa03116693262062589d14a72cc612d0&page=1&query=' + result.replace(" ", "%20")
+        url = 'https://api.themoviedb.org/3/search/movie?api_key=fa03116693262062589d14a72cc612d0&page=1&query=' + result.replace(":", "%3A").replace(" ", "%20")
         img_url = 'https://image.tmdb.org/t/p/w500'
         movie_list = get_json(url)
         movies = []
